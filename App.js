a0fbe7000d7ea19f59c831150500d4797b43abd6
Pamela Francisco <ni.pan1010@gmail.com>
2022-04-27 15:27:21 -0300

    Atividade sobre react native finalizado

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const numAleatorios = () => {
+  let num = [];
+  for (let i = 0; i < 6; i++){
+    let numSorteado = Math.floor(Math.random() * 60 + 1);
+    while(num.includes(numSorteado)){
+      numSorteado = Math.floor(Math.random() * 60 + 1);
+    }
+    num.push(numSorteado);
+  }
+  return num;
+}
+
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      texto: 'Gerador de números Mega-sena',
+      saida: numAleatorios()
+    }
+  }
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>{this.state.texto}</Text>
+        <Text>1º: {this.state.saida[0]}</Text>
+        <Text>2º: {this.state.saida[1]}</Text>
+        <Text>3º: {this.state.saida[2]}</Text>
+        <Text>4º: {this.state.saida[3]}</Text>
+        <Text>5º: {this.state.saida[4]}</Text>
+        <Text>6º: {this.state.saida[5]}</Text>
+        <Button
+          title="Clique para sortear"
+          onPress={() => {this.setState({saida: (this.saida = numAleatorios())})}}
+          />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#98FB98',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+
